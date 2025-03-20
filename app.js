f18a9a671df3550f9ce4c2e2888b8464f6027f7f
Rahuliyer918 <144077339+Rahuliyer918@users.noreply.github.com>
2025-03-20 00:54:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const reviewRoutes = require('./routes/reviews');
 
 const MongoDBStore = require("connect-mongo")(session);
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb+srv://rahuliyer918:rXvkY5pBb175PKuq@cluster0.i60bx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(dbUrl);
 
